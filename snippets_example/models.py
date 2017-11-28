4e6af21d908cddcefbac78c3ba8c6ad4571267cc
Remy Pocquerusse <remypoc@free.fr>
2017-11-28 13:21:45 +0100

    Delete file from filesystem on snippet deletion

--- snippets_example/models.py
+++ snippets_example/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
-    file = models.FileField()
+    file = models.FileField(max_length=2000)
 
     class Meta:
         ordering = ('created',)
--- snippets_example/views.py
+++ snippets_example/views.py
@@ -1,6 +1,11 @@
+import os
+from django.db import models
+from django.dispatch import receiver
+from rest_framework.parsers import MultiPartParser
+
 from snippets_example.models import Snippet
 from snippets_example.serializers import SnippetSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 
 
 # ModelViewSet provide default crud operations
@@ -8,11 +13,22 @@ class SnippetViewSet(viewsets.ModelViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
-
-    Additionally we also provide an extra `highlight` action.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    parser_classes = (MultiPartParser,)
 
     def perform_create(self, serializer):
         serializer.save()
+
+    # Used to delete files on filesystem
+    @receiver(models.signals.post_delete, sender=Snippet)
+    def auto_delete_file_on_delete(sender, instance, **kwargs):
+        """
+        Deletes file from filesystem when corresponding `MediaFile` object is deleted.
+        """
+        if instance.file:
+            if os.path.isfile(instance.file.path):
+                os.remove(instance.file.path)
+
+
